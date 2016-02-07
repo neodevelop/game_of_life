dbe8ff29e1bf75b0b163a043bf810d764799f4ff
sohjiro <felipe@makingdevs.com>
2016-02-07 15:22:09 -0600

    getting neighbors

--- lib/organism.ex
+++ lib/organism.ex
@@ -2,14 +2,26 @@ defmodule Organism do
   defstruct cells: nil
 
   def neighbors(%Organism{cells: _cells}, [x: x, y: y]) do
+    for position_x <- (x - 1)..(x + 1),
+        position_y <- (y - 1)..(y + 1),
+        position_x > -1 and position_y > -1
+        do
+        IO.puts "{x: #{position_x}, y: #{position_y}}"
+    end
+
     case {x, y} do
       {0, 0} -> [0, 0, 0]
       {0, 1} -> [0, 0, 0, 0, 0]
       {0, 2} -> [0, 0, 0]
+
       {1, 0} -> [0, 0, 0, 0, 0]
       {1, 1} -> [0, 0, 0, 0, 0, 0, 0, 0]
+      {1, 2} -> [0, 0, 0, 0, 0]
+
+      {2, 0} -> [0, 0, 0]
+      {2, 1} -> [0, 0, 0, 0, 0]
+      {2, 2} -> [0, 0, 0]
       _ -> []
     end
   end
-
 end
