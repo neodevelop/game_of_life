752e1c9e43d92e422a47746b648c70a0b86310c6
sohjiro <felipe@makingdevs.com>
2016-02-07 10:53:36 -0600

    fixing test

--- test/organism_test.exs
+++ test/organism_test.exs
@@ -2,7 +2,7 @@ defmodule Organism do
   use ExUnit.Case
   doctest Organism
 
-  test "given an organism with an initial size of 3 it should return a tuple of tuples with the same size x = y = 3" do
+  test "given an organism with an initial size of 3 it should a list of list with the same size x = y = 3" do
     organism = Organism.create(3)
     assert [[0, 0, 0], [0, 0, 0], [0, 0, 0]] = organism, "There was something wrong with initialization"
   end
