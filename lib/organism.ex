464610a693be6fdc76de11fd6694d147c5210bb0
sohjiro <felipe@makingdevs.com>
2016-02-07 15:29:47 -0600

    Mexican debugger

--- lib/organism.ex
+++ lib/organism.ex
@@ -2,12 +2,14 @@ defmodule Organism do
   defstruct cells: nil
 
   def neighbors(%Organism{cells: _cells}, [x: x, y: y]) do
+    IO.puts "cell at: {x: #{x}, y: #{y}}"
     for position_x <- (x - 1)..(x + 1),
         position_y <- (y - 1)..(y + 1),
         position_x > -1 and position_y > -1
         do
         IO.puts "{x: #{position_x}, y: #{position_y}}"
     end
+    IO.puts "**********************"
 
     case {x, y} do
       {0, 0} -> [0, 0, 0]
