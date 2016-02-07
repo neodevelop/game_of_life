90e9d53c9ae05e0b557613c4400d9ef08ec19f75
sohjiro <felipe@makingdevs.com>
2016-02-07 13:15:33 -0600

    fixing test and refactor

--- lib/organism.ex
+++ lib/organism.ex
@@ -4,6 +4,7 @@ defmodule Organism do
   def neighbors(%Organism{cells: cells}, [x: x, y: y]) do
     case {x, y} do
       {0, 0} -> [0, 0, 0]
+      {0, 1} -> [0, 0, 0, 0, 0]
       _ -> []
     end
   end
--- test/organism_test.exs
+++ test/organism_test.exs
@@ -2,18 +2,30 @@ defmodule OrganismTest do
   use ExUnit.Case
   doctest Organism
 
-  test "given an organism with an initial state of [[0, 0, 0], [0, 1, 0], [1, 0, 1]] it shuold return a new Organism structure" do
-    cells = [[0, 0, 0], [0, 1, 0], [1, 0, 1]]
+  setup do
+    {:ok, cells: [[0, 0, 0], [0, 1, 0], [1, 0, 1]]}
+  end
+
+  test "given an organism with an initial state of [[0, 0, 0], [0, 1, 0], [1, 0, 1]] it shuold return a new Organism structure", context do
+    cells = context[:cells]
     organism = %Organism{cells: cells}
     assert ^cells = organism.cells, "There was something wrong with initialization"
   end
 
-  test "given a cell in position [0, 0], it should return three neighbors" do
-    cells = [[0, 0, 0], [0, 1, 0], [1, 0, 1]]
+  test "given a cell in position [0, 0], it should return three neighbors", context do
+    cells = context[:cells]
     organism = %Organism{cells: cells}
     neighbors = Organism.neighbors(organism, [x: 0, y: 0])
 
     assert 3 = length(neighbors)
+  end
+
+  test "given a cell in position [0, 1], it should return five neighbors", context do
+    cells = context[:cells]
+    organism = %Organism{cells: cells}
+    neighbors = Organism.neighbors(organism, [x: 0, y: 1])
+
+    assert 5 = length(neighbors)
   end
 
 end
