93024be7b982453b61f73f0bce9de1001db3fdb4
sohjiro <felipe@makingdevs.com>
2016-02-07 12:30:07 -0600

    adding test

--- test/organism_test.exs
+++ test/organism_test.exs
@@ -2,18 +2,18 @@ defmodule OrganismTest do
   use ExUnit.Case
   doctest Organism
 
-  test "given an organism with an initial state of [[0, 0, 0], [0, 0, 0], [0, 0, 0]] it shuold return a new Organism structure" do
+  test "given an organism with an initial state of [[0, 0, 0], [0, 1, 0], [1, 0, 1]] it shuold return a new Organism structure" do
     cells = [[0, 0, 0], [0, 1, 0], [1, 0, 1]]
     organism = %Organism{cells: cells}
     assert ^cells = organism.cells, "There was something wrong with initialization"
   end
 
-  test "given the cell in position [0, 0], it should return three neighbors" do
+  test "given a cell in position [0, 0], it should return three neighbors" do
     cells = [[0, 0, 0], [0, 1, 0], [1, 0, 1]]
     organism = %Organism{cells: cells}
     neighbors = Organism.neighbors(organism, [x: 0, y: 0])
 
-    assert 3 = length neighbors
+    assert 3 = length(neighbors)
   end
 
 end
