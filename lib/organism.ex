b9319e408475801ae0ca011f40455d6b8b384830
sohjiro <felipe@makingdevs.com>
2016-02-07 16:06:21 -0600

    calculating neighbors

--- lib/organism.ex
+++ lib/organism.ex
@@ -1,29 +1,16 @@
 defmodule Organism do
   defstruct cells: nil
 
-  def neighbors(%Organism{cells: _cells}, [x: x, y: y]) do
-    IO.puts "cell at: {x: #{x}, y: #{y}}"
+  def neighbors(%Organism{cells: cells}, [x: x, y: y]) do
     for position_x <- (x - 1)..(x + 1),
         position_y <- (y - 1)..(y + 1),
-        position_x > -1 and position_y > -1
-        do
-        IO.puts "{x: #{position_x}, y: #{position_y}}"
-    end
-    IO.puts "**********************"
-
-    case {x, y} do
-      {0, 0} -> [0, 0, 0]
-      {0, 1} -> [0, 0, 0, 0, 0]
-      {0, 2} -> [0, 0, 0]
-
-      {1, 0} -> [0, 0, 0, 0, 0]
-      {1, 1} -> [0, 0, 0, 0, 0, 0, 0, 0]
-      {1, 2} -> [0, 0, 0, 0, 0]
-
-      {2, 0} -> [0, 0, 0]
-      {2, 1} -> [0, 0, 0, 0, 0]
-      {2, 2} -> [0, 0, 0]
-      _ -> []
+        position_x in 0..(length(cells) - 1) and
+        position_y in 0..(length(cells) - 1) and
+        not(position_x == x and position_y == y)
+    do
+      cells
+      |> Enum.at(position_x)
+      |> Enum.at(position_y)
     end
   end
 end
