ca6f53ac068eba14b67009224eef8458a0130e0b
neodevelop <neodevelop@gmail.com>
2016-02-12 21:42:39 -0600

    Adding the organism script with an example

---
+++ neodevelop/lib/organism_script.rb
@@ -0,0 +1,18 @@
+require './organism'
+
+file = File.open("organism1.txt","r")
+population = file.readlines.collect do |s|
+  s.gsub('*','1').gsub('-','0').chars.collect do |n| n.to_i end
+end
+
+organism = Organism.new
+organism.feed population
+
+
+100.times do |i|
+  system "clear"
+  puts organism.show
+  organism.prepare_to_evolve
+  organism.evolve
+  sleep 0.1
+end
